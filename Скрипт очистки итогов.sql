660612bbf3db4ded98ab4c90f9a3dbc2bd483501
Vladimir Shustov <swd1986@yandex.ru>
2023-03-29 11:54:57 +0600

    Add files via upload

--- Скрипт очистки итогов.sql
+++ Скрипт очистки итогов.sql
@@ -7,6 +7,10 @@ SET @1C_offset=(select Offset from [dbo].[_YearOffset]);
 -- получим дату с учетом смещения по 1С
 SET @1C_empty_date=dateadd(year,@1C_offset,@empty_date);
 
+--
+-- цикл по трунке
+DROP TABLE IF EXISTS #truncate_table;
+
 --debug
 --select @1C_empty_date
 
@@ -15,31 +19,53 @@ with structure as(
 	t.name tablename
 	FROM        sys.columns c
 	JOIN        sys.tables  t   ON c.object_id = t.object_id
-)
+),
 
+truncate_table as (
 --справочники
-select distinct 'справочники', * from structure
-where PATINDEX('_Reference%', tablename)>0
-UNION
+select distinct 'Регистры накопления' caption, * from structure
+where PATINDEX('_AccumRg%', tablename)>0
+--UNION
 --Документы
-select distinct 'Документы', * from structure
-where PATINDEX('_Document%', tablename)>0
-UNION
+--select distinct 'Документы', * from structure
+--where PATINDEX('_Document%', tablename)>0
+--UNION
 --Регистры сведений
-select distinct 'Регистры сведений', * from structure
-where PATINDEX('_InfoRg%', tablename)>0
-UNION
+--select distinct 'Регистры сведений', * from structure
+--where PATINDEX('_InfoRg%', tablename)>0
+--UNION
 --Последовательности документов
-select distinct 'Последовательности документов',* from structure
-where PATINDEX('_Seq%', tablename)>0
-UNION
---Журналы документов
-select distinct 'Журналы документов',* from structure
-where PATINDEX('_DocumentJournal%', tablename)>0
+--select distinct 'Последовательности документов',* from structure
+--where PATINDEX('_Seq%', tablename)>0
+--UNION
+----Журналы документов
+--select distinct 'Журналы документов',* from structure
+--where PATINDEX('_DocumentJournal%', tablename)>0
 --select * from [dbo].[DBSchema]
 --select * from [dbo].[_YearOffset]
+)
 
-                              tablename
------------------------------ --------------------------------------------------------------------------------------------------------------------------------
-Регистры сведений             _InfoRg35
-справочники                   _Reference31
+select tablename into #truncate_table from truncate_table;
+-- cursor
+DECLARE @cursor CURSOR;
+DECLARE @t		NVARCHAR(max);
+SET @cursor = CURSOR FOR
+select * from #truncate_table
+
+OPEN @cursor
+FETCH NEXT
+FROM @cursor INTO @t
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	PRINT 'Трункаем таблицу: ' + @t
+	declare @sql nvarchar(max);
+	set @sql = 'TRUNCATE TABLE '+ @t +';'
+	EXEC (@Sql);
+	
+	FETCH NEXT
+	FROM @cursor  INTO @t
+END
+
+CLOSE @cursor
+DEALLOCATE @cursor
+
